77efdfa2375aa53b93ae9d6945db10a0db0c73c3
Anatolii Kryvda <anatolii.kryvda@gmail.com>
2026-06-19 22:52:03 +0300

    Initial empty version

---
+++ src/main/java/just/demo/SpringGrpcApplication.java
@@ -0,0 +1,13 @@
+package just.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGrpcApplication.class, args);
+	}
+
+}

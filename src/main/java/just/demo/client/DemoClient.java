0fdd2e149dae4dc5c6b2dc15ad004bfa3df1d05e
Anatolii Kryvda <anatolii.kryvda@gmail.com>
2026-06-20 00:42:09 +0300

    Simplify client

---
+++ src/main/java/just/demo/client/DemoClient.java
@@ -0,0 +1,156 @@
+package just.demo.client;
+
+import io.grpc.Channel;
+import io.grpc.stub.ClientCallStreamObserver;
+import io.grpc.stub.ClientResponseObserver;
+import io.grpc.stub.StreamObserver;
+import just.demo.proto.DemoRequest;
+import just.demo.proto.DemoResponse;
+import just.demo.proto.DemoServiceGrpc;
+import just.demo.proto.DemoServiceGrpc.DemoServiceStub;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.grpc.client.GrpcChannelFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.util.stream.IntStream.rangeClosed;
+
+@SpringBootApplication
+@SuppressWarnings("LoggingSimilarMessage")
+public class DemoClient implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(DemoClient.class);
+
+    private final GrpcChannelFactory channels;
+
+    public DemoClient(GrpcChannelFactory channels) {
+        this.channels = channels;
+    }
+
+    public static void main(String[] args) {
+        System.exit(SpringApplication.exit(new SpringApplicationBuilder(DemoClient.class)
+                .web(WebApplicationType.NONE)
+                .properties("spring.grpc.server.enabled=false")
+                .run(args)));
+    }
+
+    @Override
+    public void run(String... args) throws InterruptedException {
+        Channel channel = channels.createChannel("localhost:9090");
+
+        LOG.info("Unary...");
+        runUnary(channel);
+        LOG.info("Server streaming...");
+        runServerStream(channel);
+        LOG.info("Client streaming...");
+        runClientStream(channel);
+        LOG.info("Bidirectional streaming...");
+        runBidiStream(channel);
+    }
+
+    private static void runUnary(Channel channel) {
+        DemoResponse response = DemoServiceGrpc.newBlockingStub(channel)
+                .unary(DemoRequest.newBuilder().setText("Demo request").build());
+        LOG.info("response: {}", response.getText());
+    }
+
+    private static void runServerStream(Channel channel) throws InterruptedException {
+        CountDownLatch done = new CountDownLatch(1);
+        DemoServiceStub stub = DemoServiceGrpc.newStub(channel);
+        stub.serverStream(DemoRequest.newBuilder().setText("Demo request").build(), new StreamObserver<>() {
+            @Override
+            public void onNext(DemoResponse response) {
+                LOG.info("response: {}", response.getText());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                LOG.error("error: {}", t.getMessage());
+                done.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                LOG.info("completed");
+                done.countDown();
+            }
+        });
+        done.await();
+    }
+
+    private static void runClientStream(Channel channel) throws InterruptedException {
+        CountDownLatch done = new CountDownLatch(1);
+        DemoServiceStub stub = DemoServiceGrpc.newStub(channel);
+        StreamObserver<DemoRequest> requestObserver = stub.clientStream(new StreamObserver<>() {
+            @Override
+            public void onNext(DemoResponse response) {
+                LOG.info("response: {}", response.getText());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                LOG.error("error: {}", t.getMessage());
+                done.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                LOG.info("completed");
+                done.countDown();
+            }
+        });
+        rangeClosed(1, 10).forEach(counter -> {
+            requestObserver.onNext(DemoRequest.newBuilder().setText("Demo request " + counter).build());
+        });
+        requestObserver.onCompleted();
+        done.await();
+    }
+
+    private static void runBidiStream(Channel channel) throws InterruptedException {
+        CountDownLatch done = new CountDownLatch(1);
+        DemoServiceStub stub = DemoServiceGrpc.newStub(channel);
+        AtomicInteger counter = new AtomicInteger();
+        stub.bidiStream(new ClientResponseObserver<DemoRequest, DemoResponse>() {
+            private ClientCallStreamObserver<DemoRequest> requestStream;
+
+            @Override
+            public void beforeStart(ClientCallStreamObserver<DemoRequest> requestStream) {
+                this.requestStream = requestStream;
+                requestStream.setOnReadyHandler(this::next);
+            }
+
+            @Override
+            public void onNext(DemoResponse response) {
+                LOG.info("response: {}", response.getText());
+                next();
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                LOG.error("error: {}", t.getMessage());
+                done.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                LOG.info("completed");
+                done.countDown();
+            }
+
+            private void next() {
+                if (counter.incrementAndGet() > 10) {
+                    requestStream.onCompleted();
+                } else {
+                    requestStream.onNext(DemoRequest.newBuilder().setText("Demo request " + counter.get()).build());
+                }
+            }
+        });
+        done.await();
+    }
+}

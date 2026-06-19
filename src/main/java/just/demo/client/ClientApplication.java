b10e6abcd18b43d199cca356086c53ab901ac51f
Anatolii Kryvda <anatolii.kryvda@gmail.com>
2026-06-20 02:23:47 +0300

    Simplify client

--- src/main/java/just/demo/client/ClientApplication.java
+++ src/main/java/just/demo/client/ClientApplication.java
@@ -7,12 +7,12 @@ import just.demo.proto.DemoRequest;
 import just.demo.proto.DemoResponse;
 import just.demo.proto.DemoServiceGrpc.DemoServiceBlockingStub;
 import just.demo.proto.DemoServiceGrpc.DemoServiceStub;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.grpc.client.ImportGrpcClients;
 
 import java.util.concurrent.CountDownLatch;
@@ -22,13 +22,9 @@ import static java.util.stream.IntStream.rangeClosed;
 
 @Slf4j
 @SpringBootApplication
-@RequiredArgsConstructor
 @SuppressWarnings("LoggingSimilarMessage")
 @ImportGrpcClients({DemoServiceBlockingStub.class, DemoServiceStub.class})
-public class ClientApplication implements CommandLineRunner {
-
-    private final DemoServiceBlockingStub blockingStub;
-    private final DemoServiceStub asyncStub;
+public class ClientApplication {
 
     public static void main(String[] args) {
         System.exit(SpringApplication.exit(new SpringApplicationBuilder(ClientApplication.class)
@@ -36,16 +32,18 @@ public class ClientApplication implements CommandLineRunner {
                 .run(args)));
     }
 
-    @Override
-    public void run(String... args) throws InterruptedException {
-        log.info("Unary...");
-        runUnary(blockingStub);
-        log.info("Server streaming...");
-        runServerStream(asyncStub);
-        log.info("Client streaming...");
-        runClientStream(asyncStub);
-        log.info("Bidirectional streaming...");
-        runBidirectionalStream(asyncStub);
+    @Bean
+    CommandLineRunner run(DemoServiceBlockingStub blockingStub, DemoServiceStub asyncStub) {
+        return args -> {
+            log.info("Unary...");
+            runUnary(blockingStub);
+            log.info("Server streaming...");
+            runServerStream(asyncStub);
+            log.info("Client streaming...");
+            runClientStream(asyncStub);
+            log.info("Bidirectional streaming...");
+            runBidirectionalStream(asyncStub);
+        };
     }
 
     private static void runUnary(DemoServiceBlockingStub stub) {

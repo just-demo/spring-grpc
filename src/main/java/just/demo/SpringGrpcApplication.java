7fb6f81a102d860866a9593bf95bce5da89d4f66
Anatolii Kryvda <anatolii.kryvda@gmail.com>
2026-06-20 02:07:29 +0300

    Separate server config

--- src/main/java/just/demo/SpringGrpcApplication.java
+++ src/main/java/just/demo/SpringGrpcApplication.java
@@ -1,13 +1,15 @@
 package just.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(scanBasePackages = "just.demo.service")
 public class SpringGrpcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringGrpcApplication.class, args);
+        new SpringApplicationBuilder(SpringGrpcApplication.class)
+                .profiles("server")
+                .run(args);
     }
 
 }
